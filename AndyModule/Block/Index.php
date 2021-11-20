bbb914bef68f4c632270700932eb7dad499e0469
Andy <andyacedoc@gmail.com>
2021-11-20 20:59:08 +0300

    Задание 3

--- AndyModule/Block/Index.php
+++ AndyModule/Block/Index.php
@@ -2,12 +2,46 @@
 
 namespace Amasty\AndyModule\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
 
 class Index extends Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        Template\Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+
     public function renderA(string $f)
-        {
-            return 'Hi, ' . $f;
+    {
+        return 'Hi, ' . $f;
+    }
+
+    public function welcomeText()
+    {
+        return $this->scopeConfig->getValue('andy_config/general/welcome_text') ?: 'NothinG.';
+    }
+
+    public function showInputQty()
+    {
+        $showinput = '';
+        if ($this->scopeConfig->isSetFlag('andy_config/general/qty_enabled')) {
+            $class_css = $this->hasData("css_class_3") ? $this->getData("css_class_3") : "";
+            $valuedefault = $this->scopeConfig->getValue('andy_config/general/qty_value');
+            $showinput = 'Количество:<br><input type="number" class='
+                    . $class_css . ' id="qtyid" name="qty" value="'
+                    . $valuedefault . '"><br>';
         }
+        return $showinput;
+    }
 }
--- AndyModule/Controller/Index/Index.php
+++ AndyModule/Controller/Index/Index.php
@@ -3,13 +3,75 @@
 namespace Amasty\AndyModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+//
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+
 
 class Index extends Action
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+    /**
+     * @var CheckoutSession
+     */
+    private $checkoutSession;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+    /**
+     * @var ProductCollectionFactory
+     */
+    private $productCollectionFactory;
+
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig,
+        CheckoutSession $checkoutSession,
+        ProductRepositoryInterface $productRepository,
+        ProductCollectionFactory $productCollectionFactory
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->checkoutSession = $checkoutSession;
+        $this->productRepository = $productRepository;
+        $this->productCollectionFactory = $productCollectionFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        //exit('whatwhat');
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+//        $qoute = $this->checkoutSession->getQuote();
+//        if(!$qoute->getId()) {
+//            $qoute->save();
+//        }
+//
+//        $product = $this->productRepository->get('24-MB04');
+//        $qoute->addProduct($product,5);
+//        $qoute->save();
+//        exit('kwota');
+
+//        $collection = $this->productCollectionFactory->create();
+//        $collection->addAttributeToFilter('sku', '24-MB01');
+//        $collection->addAttributeToSelect('name');
+//        $collection->addAttributeToSelect('description');
+//
+//        foreach ($collection as $product) {
+//            echo $product->getName();
+//            echo $product->getDescription();
+//        }
+//        exit('productlist');
+
+        if ($this->scopeConfig->isSetFlag('andy_config/general/enabled')) {
+            return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        } else {
+            exit('Модуль выключен.');
+        }
     }
 }
